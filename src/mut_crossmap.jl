1e560d0be333e416d92590fa40ede872a45d2956
PierreBarrat <p.barrat@live.fr>
2021-05-14 17:09:37 +0200

    mod ancestral_sequences!

--- src/mut_crossmap.jl
+++ src/mut_crossmap.jl
@@ -1,10 +1,20 @@
 """
-	function mut_crossmap_to_tree!(t::Tree, json::AbstractString, segment; cmkey=cmmuts selfmutkey = :selfmuts jsonkey=:aa_muts)
+	mut_crossmap_to_tree!(t::Tree, json::AbstractString, segment;
+		cmkey=:cmmuts,
+		selfmutkey=:selfmuts,
+		jsonkey=:aa_muts,
+		muttype=Char,
+	)
 
-Add mutation crossmapping `json` file from augur to `t`. Data is added to `n.data.dat[cmkey][s]` for each tree node `n` and segment `s != segment` (read from the json file). 
-Mutations for key `segment` are added to `n.data.dat[selfmutkey]`. 
+Add mutation crossmapping `json` file from augur to `t`. Data is added to `n.data.dat[cmkey][s]` for each tree node `n` and segment `s != segment` (read from the json file).
+Mutations for key `segment` are added to `n.data.dat[selfmutkey]`.
 """
-function mut_crossmap_to_tree!(t::Tree, json::AbstractString, segment; cmkey=:cmmuts, selfmutkey = :selfmuts, jsonkey=:aa_muts, muttype=Char)
+function mut_crossmap_to_tree!(t::Tree, json::AbstractString, segment;
+		cmkey=:cmmuts,
+		selfmutkey = :selfmuts,
+		jsonkey=:aa_muts,
+		muttype=Char,
+	)
 	cm = JSON3.read(read(json, String), Dict)["nodes"]
 	# Look for `jsonkey` in cm and map it to tree
 	TreeTools.map_dict_to_tree!(t, cm, key=jsonkey)
@@ -12,14 +22,14 @@ function mut_crossmap_to_tree!(t::Tree, json::AbstractString, segment; cmkey=:cm
 		n.data.dat[cmkey] = Dict(s => [TreeTools.parse_mutation(m, muttype) for m in M] for (s,M) in n.data.dat[jsonkey])
 		n.data.dat[selfmutkey] = n.data.dat[cmkey][segment]
 		delete!(n.data.dat[cmkey], segment)
-		delete!(n.data.dat, jsonkey) 
+		delete!(n.data.dat, jsonkey)
 	end
 end
 
 """
 	fasta_crossmap_to_trees!(f::Function, trees::Dict, selfkey=:selfseq, cmkey=:cmseq)
 
-Store sequences of several fasta files onto trees. For each pair `(s, t)` of `trees`: 
+Store sequences of several fasta files onto trees. For each pair `(s, t)` of `trees`:
 - Sequences in fasta file `f(s)` are stored on leaves of `t` under `n.data.dat[selfkey]`
 - For each other key `σ` of `trees`, sequences in fasta file `f(σ)` are stored on leaves of `t` under `n.data.dat[cmkey][σ]`.
 """
@@ -33,31 +43,57 @@ function fasta_crossmap_to_trees!(f::Function, trees::Dict, selfkey=:selfseq, cm
 end
 
 """
-	function ancestral_sequences!(trees::Dict)
+	crossmap_sequences!(trees::Dict, selfkey = :selfseq, cmkey = :cmseq)
+
+Map sequences of the leaves of each tree to the leaves of all others.
 """
-function ancestral_sequences!(trees::Dict; self=true, crossmapped=true)
+function crossmap_sequences!(trees::Dict, selfkey = :selfseq, cmkey = :cmseq)
 	for (sref,tref) in trees
-		# self sequences and mutations
-		self && self_ancestral_states!(tref)
-		# crossmapped sequences and mutations
-		crossmapped && crossmapped_ancestral_states!(trees, sref)
+		for n in leaves(tref)
+			n.data.dat[cmkey] = Dict()
+			for s in Iterators.filter(!=(sref), keys(trees))
+				n.data.dat[cmkey][s] = trees[s].lleaves[n.label].data.dat[selfkey]
+			end
+		end
 	end
 end
 
-function self_ancestral_states!(tree, key=:selfseq)
-	TreeAlgs.Fitch.fitch_mutations!(tree, :selfmuts, key)
-	# TreeTools.compute_mutations!(tree, :selfmuts) do n
-	# 	n.isleaf ? n.data.dat[key] : n.data.dat[:selfseq_ancestral] 
-	# end
+"""
+	ancestral_sequences!(trees::Dict;
+		self = true,
+		selfkey_seq = :selfseq,
+		selfkey_muts = :selfmuts,
+		crossmapped = true,
+		cmkey_seq = :cmseq,
+		cmkey_muts = :cmmuts,
+	)
+
+Compute ancestral *mutations* using Fitch algorithm. (*Note*: name should be changed).
+"""
+function ancestral_sequences!(trees::Dict;
+		self = true,
+		selfkey_seq = :selfseq,
+		selfkey_muts = :selfmuts,
+		crossmapped = true,
+		cmkey_seq = :cmseq,
+		cmkey_muts = :cmmuts,
+	)
+	for (sref,tref) in trees
+		# self sequences and mutations
+		self && self_ancestral_states!(tref, selfkey_seq, selfkey_muts)
+		# crossmapped sequences and mutations
+		crossmapped && crossmapped_ancestral_states!(trees, sref, cmkey_seq, cmkey_muts)
+	end
 end
 
-function crossmapped_ancestral_states!(trees, sref, key=:cmseq)
+function self_ancestral_states!(tree, seqkey=:selfseq, mutkey=:selfmuts)
+	TreeAlgs.Fitch.fitch_mutations!(tree, mutkey, seqkey)
+end
+
+function crossmapped_ancestral_states!(trees, sref, seqkey=:cmseq, mutkey=:cmmuts)
 	tref = trees[sref]
 	for s in Iterators.filter(!=(sref), keys(trees))
-		TreeAlgs.Fitch.fitch_mutations!(tref, (:cmmuts, s), (key, s))
-		# TreeTools.compute_mutations!(tref, (:cmmuts,s)) do n
-		# 	n.isleaf ? n.data.dat[key][s] : n.data.dat[:cmseq_ancestral][s] 
-		# end
+		TreeAlgs.Fitch.fitch_mutations!(tref, (mutkey, s), (seqkey, s))
 	end
 end
 
@@ -65,10 +101,10 @@ end
 """
 	get_mut_dict(f::Function, tree:Tree)
 
-Construct dictionary mapping mutations of segment `s` in tree `t` to the number of times they appear. For `n` in `t.lnodes`, looks for mutations in `f(n)`. 
+Construct dictionary mapping mutations of segment `s` in tree `t` to the number of times they appear. For `n` in `t.lnodes`, looks for mutations in `f(n)`.
 """
 function get_mut_dict(f::Function, t::Tree)
-	md = Dict{TreeTools.Mutation,Int64}() 
+	md = Dict{TreeTools.Mutation,Int64}()
 	for n in values(t.lnodes)
 		for m in f(n)
 			md[TreeTools.parse_mutation(m)] = get(md, TreeTools.parse_mutation(m), 0) + 1
@@ -80,7 +116,7 @@ end
 """
     suspicious_mutations(md_ref, md_new ; n = 100)
 
-Following mutations `m` are suspicious: 
+Following mutations `m` are suspicious:
 - `m` appears in `md_new` but not in `md_ref`
 - `m` appears only once in `md_ref`, but multiple times in `md_new`
 """
@@ -101,18 +137,18 @@ end
 """
 	suspicious_branches!(trees::Dict{T,<:Tree}, cmkey=cmmuts where T
 
-Mark suspicious branches in `trees` using cross-mapped mutations. Store the number of suspicious mutations for key `s` on the branch above tree node `n` in `n.data.dat[:suspicious_muts][s]`. 
+Mark suspicious branches in `trees` using cross-mapped mutations. Store the number of suspicious mutations for key `s` on the branch above tree node `n` in `n.data.dat[:suspicious_muts][s]`.
 """
 function suspicious_branches!(trees::Dict{T,<:Tree}, selfmutkey = :selfmuts, cmkey=:cmmuts) where T
 	# Set of mutations on branches for segment s on its own tree
 	mut_dict = Dict(s=>get_mut_dict(n -> n.data.dat[selfmutkey], t) for (s,t) in trees)
-	# 
+	#
 	for (s,t) in trees
 		for n in values(t.lnodes)
-			n.data.dat[:suspicious_muts] = Dict{T, Int64}()	
+			n.data.dat[:suspicious_muts] = Dict{T, Int64}()
 		end
 	end
-	# 
+	#
 	for (sref,tref) in trees, s in Iterators.filter(!=(sref), keys(trees))
 		_suspicious_branches!(tref, s, mut_dict, cmkey)
 	end
@@ -167,7 +203,9 @@ function _prune_suspicious_mccs!(trees, suspmut_key=:suspicious_muts)
 	# Prune those that do
 	for i in idx
 		for (s,t) in trees
-			prunesubtree!(t, mccs[i])
+			if !lca(t, mccs[i]).isroot
+				prunesubtree!(t, mccs[i])
+			end
 		end
 	end
 	# Return pruned mccs
