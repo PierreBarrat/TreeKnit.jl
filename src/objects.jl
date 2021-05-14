189a118fe431bf383a2bf740c0dcd458397a7926
PierreBarrat <p.barrat@live.fr>
2021-05-14 17:10:12 +0200

    edits

--- src/objects.jl
+++ src/objects.jl
@@ -4,13 +4,13 @@
 Storing parameters for `SplitGraph.runopt` function.
 
 ### General
-- `γ::Real = 3`   
+- `γ::Real = 3`
 - `itmax::Int64 = 15`: Maximal number of iterations of MCC / SA cycles
-- `likelihood_sort::Bool = true`: sort equivalent configurations using likelihood test (based on branch length for now). 
-- `resolve::Bool = true`: try to resolve trees while finding MCCs. 
+- `likelihood_sort::Bool = true`: sort equivalent configurations using likelihood test (based on branch length for now).
+- `resolve::Bool = true`: try to resolve trees while finding MCCs.
 - `seq_lengths = ones(Int64, 2)`: lengths of sequences that trees were built from
-- `crossmap_resolve::Bool = false`: Use cross-mapped mutations to resolve polytomies in the tree.  Need each leaf's data to have `:selfseq` and `:cmseq` entries.
-- `crossmap_prune::Bool = false`: Use cross-mappped mutations to prune MCCs preventively. The code will look at the number of suspicious mutations at `n.data.dat[:suspicious_muts][s]` where `n = trees[s]` (`s` is assumed to be an influenza segment). Need each leaf's data to have `:selfseq` and `:cmseq` entries.
+- `crossmap_resolve::Bool = false`: Use cross-mapped mutations to resolve polytomies in the tree.  Require each leaf's data to have `:selfseq` and `:cmseq` entries.
+- `crossmap_prune::Bool = false`: Use cross-mappped mutations to prune MCCs preventively. The code will look at the number of suspicious mutations at `n.data.dat[:suspicious_muts][s]` where `n = trees[s]` (`s` is assumed to be an influenza segment). Require each leaf's data to have `:selfseq` and `:cmseq` entries.
 ### Simulated annealing
 - `Md::Real = 10`:  Number of SA iterations (per temperature) for a tree of `n` leaves is `ceil(Int64, n/Md)`
 - `Tmin::Float64 = 1e-3`: Minimal temperature of SA
@@ -30,8 +30,8 @@ Storing parameters for `SplitGraph.runopt` function.
 	seq_lengths = ones(Int64, 2)
 	crossmap_resolve::Bool = false
 	crossmap_prune::Bool = false
-	# For the annealing  
-	Md::Real = 10 
+	# For the annealing
+	Md::Real = 10
 	Tmin::Float64 = 1e-3
 	Tmax::Float64 = 1.; @assert Tmax > Tmin
 	dT::Float64 = 1e-2
