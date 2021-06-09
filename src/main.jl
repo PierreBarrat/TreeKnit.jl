f281dd35dc58302fd58b0fd2e73392147e0dd5bf
PierreBarrat <p.barrat@live.fr>
2021-06-09 18:18:50 +0200

    fix bug in stop_conditions! when itmax was reached

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,6 @@
 """
 	computeMCCs(
-		trees::Dict{<:Any, <:Tree}, oa::OptArgs=OptArgs();
+		trees::Dict{<:Any, <:Tree}[, oa::OptArgs=OptArgs()];
 		preresolve=true, naive=false
 	)
 
@@ -18,12 +18,14 @@ Controls parameters of the MCC inference (unless `naive=true`). See `?OptArgs` f
 - Else, only the first pass is performed. For more than two trees, this may find MCCs that
   introduce incompatible splits in case of poorly resolved input trees.
 
+In general, this should be set to `true` if more than two trees are used, and to `false`
+  for only two trees.
+
 ### `naive = false`
 - If `true`, use a naive estimation for MCCs, *i.e.* find all clades that have an exactly
   matching topology in all trees.
 - Else, use a pseudo-parsimonious method based (mostly) on topology. The method
   `runopt(oa,t1,t2)` is called on every pair of trees.
-  by calling function `runopt(oa,t1,t2)` on pairs of trees. Return MCCs and resolved splits.
 
 ### `output = :mccs`
 Control the type of output. If `:mccs`, only MCCs are returned. If `:all`, splits added
@@ -158,9 +160,10 @@ function runopt(oa::OptArgs, trees::Dict)
 	SplitGraph.set_vverbose(oa.vv)
 	SplitGraph.set_resolve(oa.resolve)
 
-	for i in 1:oa.itmax
+	it = 1
+	while true
 		flag = :init
-		oa.verbose && println("\n --- \nIteration $i/$(oa.itmax) - $(length(leaves(first(values(ot))))) leaves remaining")
+		oa.verbose && println("\n --- \nIteration $it/$(oa.itmax) - $(length(leaves(first(values(ot))))) leaves remaining")
 
 		# Topology based inference
 		oa.verbose && println("\n## Running optimization to find MCCs...")
@@ -177,7 +180,7 @@ function runopt(oa::OptArgs, trees::Dict)
 
 		# Stopping condition
 		oa.verbose && println("\n## Proceeding based on newly found MCCs...")
-		flag, rMCCs = stop_conditions!(MCCs, mccs, oa, values(ot)...; hardstop=true)
+		flag, rMCCs = stop_conditions!(MCCs, mccs, oa, it, values(ot)...; hardstop=true)
 
 		# Checks
 		!prod([check_tree(t) for t in values(ot)]) && @error "Problem in a tree"
@@ -188,6 +191,7 @@ function runopt(oa::OptArgs, trees::Dict)
 			mccs, MCCs, rMCCs, :topology_optimization
 		)
 		(flag == :stop) && break
+		it += 1
 	end
 
 	# Output
@@ -202,13 +206,13 @@ function runopt(oa::OptArgs, trees::Dict)
 	end
 end
 
-function stop_conditions!(previous_mccs, new_mccs, oa, trees... ; hardstop=true)
+function stop_conditions!(previous_mccs, new_mccs, oa, it, trees... ; hardstop=true)
 	# If no solution was found
 	if length(new_mccs) == 0
 		oa.verbose && print("No solution found... ")
 		remaining_mccs = naive_mccs(trees)
-		## If hardstop,  stop
-		if hardstop
+		## If hardstop or maxit,  stop
+		if hardstop || it > oa.itmax
 			append!(previous_mccs, remaining_mccs)
 			oa.verbose && println("Stopping.")
 			return :stop, []
@@ -235,6 +239,11 @@ function stop_conditions!(previous_mccs, new_mccs, oa, trees... ; hardstop=true)
 		append!(new_mccs, remaining_mccs)
 		append!(previous_mccs, remaining_mccs)
 		oa.verbose && println("Resulting trees are compatible: final decomposition found. Stopping.")
+		return :stop, []
+	### If we reached the maximum number of iterations, stop
+	elseif it > oa.itmax
+		append!(previous_mccs, remaining_mccs)
+		oa.verbose && println("Maximum number of iterations reached. Stopping.")
 		return :stop, []
 	### Otherwise, continue
 	else
