ec2e639f41323649a722972ef8fbef9b2490da08
PierreBarrat <p.barrat@live.fr>
2021-06-28 13:51:53 +0200

    add mode to write_mccs

--- src/mcc_IO.jl
+++ src/mcc_IO.jl
@@ -12,25 +12,35 @@ Write MCCs to file.
 function write_mccs(
 	outfile::AbstractString,
 	MCCs::AbstractDict,
+	mode="w";
 	segments = get_segments(MCCs),
 )
-	write_mccs((s1,s2) -> outfile, MCCs, segments)
+	write_mccs((s1,s2) -> outfile, MCCs, mode; segments)
 end
-function write_mccs(outfile::Function, MCCs::AbstractDict, segments = get_segments(MCCs))
+function write_mccs(
+	outfile::Function,
+	MCCs::AbstractDict,
+	mode = "w";
+	segments = get_segments(MCCs),
+)
 	for i in 1:length(segments), j in (i+1):length(segments)
-		write_mccs(outfile(segments[i], segments[j]), MCCs[segments[i], segments[j]])
+		write_mccs(outfile(segments[i], segments[j]), MCCs[segments[i], segments[j]], mode)
 	end
 
 	return nothing
 end
 
-function write_mccs(of, MCCs::AbstractArray)
-	open(of, "w") do w
-		for m in MCCs
+function write_mccs(of, MCCs::AbstractArray, mode="w")
+	open(of, mode) do w
+		for (i,m) in enumerate(MCCs)
 			for x in m[1:end-1]
 				write(w, x * ",")
 			end
-			write(w, m[end] * "\n")
+			if i < length(MCCs)
+				write(w, m[end] * "\n")
+			else
+				write(w, m[end])
+			end
 		end
 	end
 
