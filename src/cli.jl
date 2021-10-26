bb28b38e36dd4c279b3dab7bcb1b4fd5f81717af
PierreBarrat <p.barrat@live.fr>
2021-10-26 11:05:30 +0200

    work on cli

--- src/cli.jl
+++ src/cli.jl
@@ -22,20 +22,20 @@ treeknit
   different MCCs.
 - `--no-resolve`: Do not attempt to resolve trees before inferring MCCs.
 """
-@cast function treeknit(
+@main function treeknit(
 	nwk1::AbstractString, nwk2::AbstractString;
 	# options
 	outdir::AbstractString = "treeknit_results",
-	gamma = 2.,
+	gamma::Float64 = 2.,
 	seq_lengths::AbstractString = "1 1",
-	n_sa_it = 0.1,
+	n_sa_it::Float64 = 0.1,
 	# flags
 	naive::Bool = false,
 	no_likelihood::Bool = false,
 	no_resolve::Bool = false,
 )
 	println("Treeknit: ")
-	println("Input trees: $nwk1 - $nwk2")
+	println("Input trees: $nwk1 \t $nwk2")
 	println("Results directory: $outdir")
 	println("γ: $gamma")
 
@@ -61,36 +61,62 @@ treeknit
 
 	# Write output
 	write_mccs(outdir * "/" * "MCCs.dat", MCCs)
+	write_newick(outdir * "/" * nwk1 * "_resolved", t1)
+	write_newick(outdir * "/" * nwk2 * "_resolved", t2)
+	write(outdir * "/" * "arg.nwk", arg)
+	write_rlm(outdir * "/" * "nodes.dat", rlm)
 end
 
-@cast function tmp(;x = "1 1")
-	xn = map(i->parse(Int, i), split(x, " "))
-	println(xn[1])
-	println(xn[2])
+function write_rlm(filename, rlm)
+	open(filename, "w") do io
+		for (n, (k,v)) in enumerate(rlm)
+			write(io, k)
+			write(io, ",")
+			for (i,vv) in enumerate(v)
+				if isnothing(vv)
+					write(io, " ")
+				else
+					write(io, vv)
+				end
+				if i != length(v)
+					write(io, ",")
+				end
+			end
+			if n != length(rlm)
+				write(io, "\n")
+			end
+		end
+	end
 end
 
-"""
-ArgParse example implemented in Comonicon.
+# @cast function tmp(;x = "1 1")
+# 	xn = map(i->parse(Int, i), split(x, " "))
+# 	println(xn[1])
+# 	println(xn[2])
+# end
 
-# Arguments
+# """
+# ArgParse example implemented in Comonicon.
 
-- `x`: an argument
+# # Arguments
 
-# Options
+# - `x`: an argument
 
-- `--opt1 <arg>`: an option
-- `-o, --opt2 <arg>`: another option
+# # Options
 
-# Flags
+# - `--opt1 <arg>`: an option
+# - `-o, --opt2 <arg>`: another option
 
-- `-f, --flag`: a flag
-"""
-@cast function example1(x; opt1=1, opt2::Int=2, flag=false)
-    println("Parsed args:")
-    println("flag=>", flag)
-    println("arg=>", x)
-    println("opt1=>", opt1)
-    println("opt2=>", opt2)
-end
+# # Flags
 
-@main
+# - `-f, --flag`: a flag
+# """
+# @cast function example1(x; opt1=1, opt2::Int=2, flag=false)
+#     println("Parsed args:")
+#     println("flag=>", flag)
+#     println("arg=>", x)
+#     println("opt1=>", opt1)
+#     println("opt2=>", opt2)
+# end
+
+# @main
