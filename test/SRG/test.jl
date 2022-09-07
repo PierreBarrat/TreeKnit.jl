13279480dba89a9ab40fbee6963d39aec027fb1c
PierreBarrat <p.barrat@live.fr>
2022-09-07 17:30:02 +0200

    Only use the random args test if ARGTools is installed

--- test/SRG/test.jl
+++ test/SRG/test.jl
@@ -1,3 +1,7 @@
 include("$(dirname(pathof(TreeKnit)))/..//test/SRG/fix_shared_singletons.jl")
 include("$(dirname(pathof(TreeKnit)))/..//test/SRG/construct_arg.jl")
-include("$(dirname(pathof(TreeKnit)))/..//test/SRG/random_args.jl")
+
+using Pkg
+if "ARGTools" in keys(Pkg.project().dependencies)
+	include("$(dirname(pathof(TreeKnit)))/..//test/SRG/random_args.jl")
+end
