a9778b326b30bc87c13f9bb4f43c7d1b895f89f1
anna-parker <anya.m.parker@gmail.com>
2022-09-19 21:03:43 +0200

    add to bash tests

--- test/treeknit_tests.sh
+++ test/treeknit_tests.sh
@@ -1,21 +1,37 @@
+all_tests=0
+
 julia --project=. deps/build.jl
-if [ $? -eq 0 ]; then
+retval="$?"
+if [ "$retval" == 0 ]; then
     echo Installation OK
 else
     echo Installation FAIL
+    ((all_tests++))
 fi
 
 export PATH="$HOME/.julia/bin:$PATH"
 treeknit test/NYdata/tree_ha.nwk test/NYdata/tree_na.nwk
-if [ $? -eq 0 ]; then
+retval="$?"
+if [ "$retval" == 0 ]; then
     echo CLI run OK
 else
     echo CLI run FAIL
+    ((all_tests++))
 fi
+rm -r treeknit_results
 
 julia --project=. test/runtests.jl 
-if [ $? -eq 0 ]; then
+retval="$?"
+if [ "$retval" == 0 ]; then
     echo tests OK
 else
     echo tests FAIL
+    ((all_tests++))
+fi
+
+if [ "$all_tests" == 0 ];then
+	echo "All tests passed"
+	exit 0
+else
+	exit "$all_tests"
 fi
