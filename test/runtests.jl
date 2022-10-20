67077e3e32d890e82473d8e025ad74e2a4b7ca1e
PierreBarrat <p.barrat@live.fr>
2022-10-20 18:19:10 +0200

    pretify tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,28 @@
-include("resolving/test.jl")
-include("splitgraph/test.jl")
-include("main/test.jl")
-include("SRG/test.jl")
-include("NYdata/test.jl")
+using Test
+
+@testset verbose=true "TreeKnit" begin
+	@testset "Resolving" begin
+		println("# Resolving")
+		include("resolving/test.jl")
+	end
+
+	@testset "SplitGraph" begin
+		println("# SplitGraph")
+		include("splitgraph/test.jl")
+	end
+
+	@testset "src/main.jl" begin
+		println("# Main")
+		include("main/test.jl")
+	end
+
+	@testset "SimpleReassortmentGraph" begin
+		println("# SimpleReassortmentGraph")
+		include("SRG/test.jl")
+	end
+
+	@testset "NY data" begin
+		println("# NY data")
+		include("NYdata/test.jl")
+	end
+end
