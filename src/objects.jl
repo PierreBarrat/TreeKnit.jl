abf9c8c2050e054534f5dd16e0b64aea2e479d3b
PierreBarrat <p.barrat@live.fr>
2023-05-26 15:47:31 +0200

    remove dep on Combinatorics and Setfield -- add ordered_pair commodity

--- src/objects.jl
+++ src/objects.jl
@@ -61,25 +61,27 @@ OptArgs constructor will default to :better_trees method if K>2, :better_MCCs if
 function OptArgs(K::Int; method=:None, kwargs...)
 	kwargs_dict = Dict(kwargs)
 	oa = OptArgs() #default values for K==2
-	haskey(kwargs_dict, :seq_lengths) ? oa.seq_lengths = kwargs_dict[:seq_lengths] : oa.seq_lengths = repeat([1], K)
-	haskey(kwargs_dict, :pre_resolve) ? oa.pre_resolve = kwargs_dict[:pre_resolve] : oa.pre_resolve = true
-	haskey(kwargs_dict, :strict) ? oa.strict = kwargs_dict[:strict] : oa.strict = true
+    oa.seq_lengths = get(kwargs_dict, :seq_lenghts, repeat([1], K))
+	oa.pre_resolve = get(kwargs_dict, :pre_resolve, true)
+	oa.strict = get(kwargs_dict, :strict, true)
+
 	if (method == :better_trees) || ((method == :None) && K>2)
-		haskey(kwargs_dict, :resolve) ? oa.resolve = kwargs_dict[:resolve] : oa.resolve = false
-		haskey(kwargs_dict, :final_no_resolve) ? oa.final_no_resolve = kwargs_dict[:final_no_resolve] : oa.final_no_resolve = true
-		haskey(kwargs_dict, :rounds) ? oa.rounds = kwargs_dict[:rounds] : oa.rounds = 1
+        oa.resolve = get(kwargs_dict, :resolve, false)
+        oa.final_no_resolve = get(kwargs_dict, :final_no_resolve, true)
+		oa.rounds = get(kwargs_dict, :rounds, 1)
 	elseif (method == :better_MCCs) || ((method == :None) && K==2)
-		haskey(kwargs_dict, :resolve) ? oa.resolve = kwargs_dict[:resolve] : oa.resolve = true
+        oa.resolve = get(kwargs_dict, :resolve, true)
 		if K>2
-			haskey(kwargs_dict, :final_no_resolve) ? oa.final_no_resolve = kwargs_dict[:final_no_resolve] : oa.final_no_resolve = true
-			haskey(kwargs_dict, :rounds) ? oa.rounds = kwargs_dict[:rounds] : oa.rounds = 2
+            oa.final_no_resolve = get(kwargs_dict, :final_no_resolve, true)
+			oa.rounds = get(kwargs_dict, :rounds, 2)
 		else
-			haskey(kwargs_dict, :final_no_resolve) ? oa.final_no_resolve = kwargs_dict[:final_no_resolve] : oa.final_no_resolve = false
-			haskey(kwargs_dict, :rounds) ? oa.rounds = kwargs_dict[:rounds] : oa.rounds = 1
+            oa.final_no_resolve = get(kwargs_dict, :final_no_resolve, false)
+            oa.rounds = get(kwargs_dict, :rounds, 1)
 		end
 	else
-		error("`method` should be one of `:BetterTrees`, `:BetterMCCs` or `:None`")
+		error("`method` should be one of `:BetterTrees`, `:BetterMCCs` or `:None` - got $method.")
 	end
+
 	##clean up
 	if (oa.final_no_resolve==true) && (oa.rounds==1) && (oa.resolve==true)
 		oa.resolve=false
@@ -131,7 +133,7 @@ get_linear_cooling_schedule(Tmin, Tmax, nT) = return collect(reverse(range(Tmin,
 structure to store and access computed MCCs for tree pairs
 
 - `no_trees`: number of trees
-- `order_trees`: Vector of tree.labels, order of calculations: Combinatorics.combinations(1:length(trees), 2)
+- `order_trees`: vector of tree labels.
 - `mccs`: Dictionary of calculated mccs, key is set of labels of trees in each tree pair
 
 Add and retrieve mccs with `get!` and `add!` and Tuple or Vararg of tree labels or position of tree labels in `order_trees`
@@ -153,8 +155,9 @@ function MCC_set(no_trees, order_trees, MCC_list::Vector{Vector{Vector{String}}}
     end
 
     M = MCC_set(no_trees, order_trees)
-    iters = Combinatorics.combinations(1:no_trees, 2)
-    for (i, comb) in enumerate(iters)
+    # iters = Combinatorics.combinations(1:no_trees, 2)
+    pair_iter = ordered_pairs(no_trees)
+    for (i, comb) in enumerate(pair_iter)
         add!(M, MCC_list[i], comb...)
     end
     return M
@@ -196,8 +199,8 @@ end
 Iterate over all tree pairs and their calculated MCCs in order of calculation.
 """
 function iter_pairs(M::MCC_set)
-    pairs = Combinatorics.combinations(1:M.no_trees, 2)
-    return [M.order_trees[p] for p in pairs], [get(M, p...) for p in pairs]
+    pair_iter = ordered_pairs(M.no_trees)
+    return [M.order_trees[p] for p in pair_iter], [get(M, p...) for p in pair_iter]
 end
 
 """
@@ -222,4 +225,5 @@ function Base.copy(M::MCC_set)
     return copy_M
 end
 
+ordered_pairs(K) = (([i,j] for j in (i+1):K) for i in 1:(K-1)) |> Iterators.flatten
 
